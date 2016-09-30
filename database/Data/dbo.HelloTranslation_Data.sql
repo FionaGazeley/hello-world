97ffb8f066076a3d96840057bebe8be97674f96f
unknown <fiona.gazeley@RED-GATE>
2016-09-30 15:19:35 +0100

    Add staticdata to the HelloTranslation table

---
+++ database/Data/dbo.HelloTranslation_Data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[HelloTranslation] ([Country], [Hello]) VALUES (N'France', N'Bonjour')
+INSERT INTO [dbo].[HelloTranslation] ([Country], [Hello]) VALUES (N'Germany', N'Gutten Tag')
+INSERT INTO [dbo].[HelloTranslation] ([Country], [Hello]) VALUES (N'Spain', N'Ola')
